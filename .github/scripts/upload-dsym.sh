7f7c69cdf11edded036585321ec8ca68b84ee632
Henry Moule <henry@care-planner.co.uk>
2021-06-06 21:15:46 +0100

    test changes

--- .github/scripts/upload-dsym.sh
+++ .github/scripts/upload-dsym.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+ls
 DSYM=$(find ~/Library/Developer/Xcode/Archives/ -name "*.dSYM")
 echo $DSYM
 ls
-Pods/FirebaseCrashlytics/upload-symbols -gsp ios/HealthAndMovement/GoogleService-Info.plist -p ios $DSYM
+ios/Pods/FirebaseCrashlytics/upload-symbols -gsp ios/HealthAndMovement/GoogleService-Info.plist -p ios $DSYM
