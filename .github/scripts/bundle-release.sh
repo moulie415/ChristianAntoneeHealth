1bc724190b2e7b75e95c6d4d4ef8a2b3aa19a06c
Henry Moule <hen10@hotmail.co.uk>
2021-08-09 19:26:17 +0100

    script fix

--- .github/scripts/bundle-release.sh
+++ .github/scripts/bundle-release.sh
@@ -2,6 +2,7 @@
 
 cd android/app
 echo $GOOGLE_SERVICES_JSON >> google-services.json
+echo $KEYSTORE_DATA | base64 --decode > healthandmovement.keystore
 cd ..
 echo "MYAPP_UPLOAD_KEY_ALIAS=$MYAPP_UPLOAD_KEY_ALIAS" >> gradle.properties
 echo "MYAPP_UPLOAD_STORE_FILE=$MYAPP_UPLOAD_STORE_FILE" >> gradle.properties
