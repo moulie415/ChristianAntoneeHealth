770bfd6a4dcef8b0461c44aba5710da65ed25d87
Henry Moule <hen10@hotmail.co.uk>
2021-08-09 20:45:58 +0100

    workflow fix

--- .github/scripts/bundle-release.sh
+++ .github/scripts/bundle-release.sh
@@ -8,4 +8,4 @@ echo "MYAPP_UPLOAD_KEY_ALIAS=$MYAPP_UPLOAD_KEY_ALIAS" >> gradle.properties
 echo "MYAPP_UPLOAD_STORE_FILE=$MYAPP_UPLOAD_STORE_FILE" >> gradle.properties
 echo "MYAPP_UPLOAD_STORE_PASSWORD=$MYAPP_UPLOAD_STORE_PASSWORD" >> gradle.properties
 echo "MYAPP_UPLOAD_KEY_PASSWORD=$MYAPP_UPLOAD_KEY_PASSWORD" >> gradle.properties
-./gradlew assembleRelease
+./gradlew bundleRelease
