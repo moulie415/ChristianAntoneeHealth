a671553509d1c7d2cfb7993b789fbeaa2fb910a9
Henry Moule <hen10@hotmail.co.uk>
2022-01-09 23:09:26 +0000

    messaging changes

--- app/components/views/More/Chat.tsx
+++ app/components/views/More/Chat.tsx
@@ -1,17 +1,22 @@
 import {RouteProp} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import db, {FirebaseFirestoreTypes} from '@react-native-firebase/firestore';
-import React, {useCallback, useEffect, useState} from 'react';
-import {AvatarProps, GiftedChat} from 'react-native-gifted-chat';
+import React, {useEffect, useState} from 'react';
+import {
+  GiftedChat,
+  Avatar as GiftedAvatar,
+  AvatarProps,
+} from 'react-native-gifted-chat';
 import {StackParamList} from '../../../App';
 import Profile from '../../../types/Profile';
 import {MyRootState} from '../../../types/Shared';
 import {connect} from 'react-redux';
 import {sendMessage, setMessages, setRead} from '../../../actions/profile';
 import Message from '../../../types/Message';
-import Avatar from '../../commons/Avatar';
 import {TouchableOpacity} from 'react-native';
 import moment from 'moment';
+import Avatar from '../../commons/Avatar';
+import DevicePixels from '../../../helpers/DevicePixels';
 
 interface ChatProps {
   navigation: NativeStackNavigationProp<StackParamList, 'Chat'>;
@@ -83,9 +88,27 @@ const Chat: React.FC<ChatProps> = ({
 
   return (
     <GiftedChat
-      messages={messages}
+      messages={messages.sort(
+        (a, b) => (a.createdAt as number) - (b.createdAt as number),
+      )}
       user={{_id: profile.uid, name: profile.name, avatar: profile.avatar}}
       inverted={false}
+      renderAvatar={props => {
+        return (
+          <GiftedAvatar
+            {...props}
+            renderAvatar={(p: AvatarProps<Message>) =>
+              p ? (
+                <Avatar
+                  name={p.currentMessage.user.name}
+                  src={p.currentMessage.user.avatar as string}
+                  size={DevicePixels[30]}
+                />
+              ) : null
+            }
+          />
+        );
+      }}
       onSend={msgs => {
         const message: Message = {
           ...msgs[0],
