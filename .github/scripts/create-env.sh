48d17f1ee50ec64e4d6e564a3551566f5c6cbb9a
Henry Moule <hen10@hotmail.co.uk>
2022-05-13 23:32:31 +0100

    started with rn config

---
+++ .github/scripts/create-env.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo $FACEBOOK_APP_ID >> .env
+echo $FACEBOOK_CLIENT_TOKEN >> .env
--- .github/scripts/create-sentry-files.sh
+++ .github/scripts/create-sentry-files.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-echo $SENTRY_TOKEN >> android/sentry.properties
-echo $SENTRY_TOKEN >> ios/sentry.properties
+echo $SENTRY_PROPERTIES >> android/sentry.properties
+echo $SENTRY_PROPERTIES >> ios/sentry.properties
