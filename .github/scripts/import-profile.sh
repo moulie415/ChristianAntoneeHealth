a63bd54c94974a417eee9c5dee241d6a7e0fb344
Henry Moule <hen10@hotmail.co.uk>
2022-05-27 11:30:52 +0100

    revert changes

--- .github/scripts/import-profile.sh
+++ .github/scripts/import-profile.sh
@@ -4,5 +4,3 @@ set -euo pipefail
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 echo "$PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/profile.mobileprovision
-echo "$WATCH_APP_PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/watchappprofile.mobileprovision
-echo "$WATCH_APP_EXTENSION_PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/watchappextensionprofile.mobileprovision
