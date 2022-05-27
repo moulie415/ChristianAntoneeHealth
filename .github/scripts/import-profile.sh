8e91d20b53b8af8b2451cdbc997051a1a3268856
Henry Moule <hen10@hotmail.co.uk>
2022-05-27 15:30:19 +0100

    build fix

--- .github/scripts/import-profile.sh
+++ .github/scripts/import-profile.sh
@@ -4,3 +4,5 @@ set -euo pipefail
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 echo "$PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/profile.mobileprovision
+echo "$WATCH_APP_PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/profileWatch.mobileprovision
+echo "$WATCH_APP_EXTENSION_PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/profileWatchExtension.mobileprovision
