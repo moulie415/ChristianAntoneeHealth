f21af29a18fdf1bd0ddab9c18fa601223fce6456
Henry Moule <hen10@hotmail.co.uk>
2022-08-29 11:27:41 +0100

    percentile table fixes

--- app/components/commons/PercentileTable.tsx
+++ app/components/commons/PercentileTable.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, TouchableOpacity} from 'react-native';
+import colors from '../../constants/colors';
 import DevicePixels from '../../helpers/DevicePixels';
 import {PercentileTable as PercentileTableType} from '../../types/Test';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 import Text from './Text';
+import Collapsible from 'react-native-collapsible';
+import Divider from './Divider';
 
 const CELL_WIDTH = 100;
 
@@ -20,9 +24,12 @@ const Row: React.FC<{percentile: string; table: PercentileTableType}> = ({
           textAlign: 'center',
           fontWeight: 'bold',
           fontSize: DevicePixels[12],
+          color: colors.appWhite,
+          borderColor: colors.appWhite,
         }}>
         {percentile}
       </Text>
+
       <Text
         style={{
           padding: DevicePixels[2],
@@ -31,6 +38,8 @@ const Row: React.FC<{percentile: string; table: PercentileTableType}> = ({
           textAlign: 'center',
           fontWeight: 'bold',
           fontSize: DevicePixels[12],
+          color: colors.appWhite,
+          borderColor: colors.appWhite,
         }}>
         {/* @ts-ignore */}
         {table[percentile]}
@@ -43,26 +52,44 @@ const PercentileTable: React.FC<{
   table: PercentileTableType;
   title: string;
 }> = ({table, title}) => {
+  const [collapsed, setCollapsed] = useState(true);
+
   return (
-    <View style={{margin: DevicePixels[10]}}>
-      {title && (
+    <>
+      <TouchableOpacity
+        onPress={() => setCollapsed(!collapsed)}
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}>
         <Text
           style={{
             margin: DevicePixels[10],
+            color: colors.appWhite,
           }}>
-          {title}
+          {title || 'Percentile table'}
         </Text>
-      )}
-      <Row percentile="90th" table={table} />
-      <Row percentile="80th" table={table} />
-      <Row percentile="70th" table={table} />
-      <Row percentile="60th" table={table} />
-      <Row percentile="50th" table={table} />
-      <Row percentile="40th" table={table} />
-      <Row percentile="30th" table={table} />
-      <Row percentile="20th" table={table} />
-      <Row percentile="10th" table={table} />
-    </View>
+        <Icon
+          style={{padding: DevicePixels[10]}}
+          name={collapsed ? 'plus' : 'minus'}
+          color={colors.appWhite}
+          size={DevicePixels[20]}
+        />
+      </TouchableOpacity>
+      <Collapsible style={{marginLeft: DevicePixels[10]}} collapsed={collapsed}>
+        <Row percentile="90th" table={table} />
+        <Row percentile="80th" table={table} />
+        <Row percentile="70th" table={table} />
+        <Row percentile="60th" table={table} />
+        <Row percentile="50th" table={table} />
+        <Row percentile="40th" table={table} />
+        <Row percentile="30th" table={table} />
+        <Row percentile="20th" table={table} />
+        <Row percentile="10th" table={table} />
+      </Collapsible>
+      <Divider />
+    </>
   );
 };
 
