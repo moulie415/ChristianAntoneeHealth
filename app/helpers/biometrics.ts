27dc4cf02b1b8c9342641c8ed798176fa7f6be00
Henry Moule <hen10@hotmail.co.uk>
2022-11-17 21:09:26 +0000

    save profile fixes

--- app/helpers/biometrics.ts
+++ app/helpers/biometrics.ts
@@ -468,25 +468,21 @@ export const getBodyFatPercentageSamples = async (uid: string) => {
 };
 
 export const saveBodyFatPercentage = async (value: number, uid: string) => {
-  try {
-    await db()
-      .collection('users')
-      .doc(uid)
-      .collection('bodyFatPercentage')
-      .add({value, createdate: new Date()});
-    if (Platform.OS === 'ios') {
-      return new Promise((resolve, reject) => {
-        AppleHealthKit.saveBodyFatPercentage({value}, (e, result) => {
-          if (e) {
-            reject(e);
-          } else {
-            resolve(result);
-          }
-        });
+  await db()
+    .collection('users')
+    .doc(uid)
+    .collection('bodyFatPercentage')
+    .add({value, createdate: new Date()});
+  if (Platform.OS === 'ios') {
+    return new Promise((resolve, reject) => {
+      AppleHealthKit.saveBodyFatPercentage({value}, (e, result) => {
+        if (e) {
+          reject(e);
+        } else {
+          resolve(result);
+        }
       });
-    }
-  } catch (e) {
-    console.log(e, 'bodyFatPercentage');
+    });
   }
 };
 
@@ -508,15 +504,11 @@ export const getMuscleMassSamples = async (uid: string) => {
 };
 
 export const saveMuscleMass = (value: number, uid: string) => {
-  try {
-    return db()
-      .collection('users')
-      .doc(uid)
-      .collection('muscleMass')
-      .add({value, createdate: new Date()});
-  } catch (e) {
-    console.log(e, 'muscleMass');
-  }
+  return db()
+    .collection('users')
+    .doc(uid)
+    .collection('muscleMass')
+    .add({value, createdate: new Date()});
 };
 
 export const getBoneDensitySamples = async (uid: string) => {
@@ -537,13 +529,9 @@ export const getBoneDensitySamples = async (uid: string) => {
 };
 
 export const saveBoneDensity = (value: number, uid: string) => {
-  try {
-    return db()
-      .collection('users')
-      .doc(uid)
-      .collection('boneDensity')
-      .add({value, createdate: new Date()});
-  } catch (e) {
-    console.log(e, 'boneDensity');
-  }
+  return db()
+    .collection('users')
+    .doc(uid)
+    .collection('boneDensity')
+    .add({value, createdate: new Date()});
 };
