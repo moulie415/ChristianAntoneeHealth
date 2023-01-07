4aeafcda83394374ff4722beda5f0d7f56e1dde4
Henry Moule <hen10@hotmail.co.uk>
2023-01-07 15:23:01 +0000

    minor changes

---
+++ app/components/commons/GoalSummary.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const GoalSummary = () => {
+  return (
+    <View>
+      <Text>GoalSummary</Text>
+    </View>
+  )
+}
+
+export default GoalSummary
