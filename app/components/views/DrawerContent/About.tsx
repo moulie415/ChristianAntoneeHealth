4a799504820d4a4508eed8706712c3e9ba81e3a4
Henry Moule <hen10@hotmail.co.uk>
2023-02-04 13:38:43 +0000

    minor change

--- app/components/views/DrawerContent/About.tsx
+++ app/components/views/DrawerContent/About.tsx
@@ -61,6 +61,7 @@ const About: React.FC<AboutProps> = () => {
             margin: 10,
 
             color: colors.appWhite,
+            lineHeight: 20
           }}>
           {
             'At the age of 16 Christian began his career in the health and fitness industry as a volunteer at a local health club in Ottawa, Canada. It was there that he gained an appreciation for exercise prescription and obtained a qualification as a personal trainer. The next step took him to Vancouver on the West coast of Canada where he completed an undergraduate degree in Kinesiology and Psychology.'
@@ -71,6 +72,7 @@ const About: React.FC<AboutProps> = () => {
             margin: 10,
 
             color: colors.appWhite,
+            lineHeight: 20
           }}>
           {
             'After returning to the UK Christian obtained a qualification as an Osteopath from the British College of Osteopathic Medicine and is currently pursuing a MSc in Clinical Pain management from The University of Edinburgh. By combining principles in exercise prescription, manual therapy and nutrition Christian takes a movement based approach to treating your pain and dysfunction, with a special interest in repetitive strain injuries, sports injuries and neck/low back pain.'
