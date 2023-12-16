40e64940c72c3a5b25afc61961e7b1fe503defbb
Henry Moule <henry.moule@zenobe.com>
2023-12-16 22:35:48 +0000

    test fixes

---
+++ app/helpers/sleep.test.ts
@@ -0,0 +1,10 @@
+import sleep from './sleep';
+
+describe('sleep', () => {
+  it('setTimeout should be called', async () => {
+    jest.spyOn(global, 'setTimeout');
+    await sleep(1);
+    expect(setTimeout).toHaveBeenCalled();
+  });
+
+});
