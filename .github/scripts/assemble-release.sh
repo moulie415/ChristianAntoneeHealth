ae480d3a9e57d9195c3a95d5f3b48a9ae0deb666
Henry Moule <henry.moule@zenobe.com>
2023-12-17 15:28:29 +0000

    build fixes

--- .github/scripts/assemble-release.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd android/app
-echo $GOOGLE_SERVICES_JSON >> google-services.json
-cd ..
-./gradlew assembleRelease
--- .github/scripts/upload-dsym.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ls
-DSYM=$(find ios/archive.xcarchive/dSYMs -name "*.dSYM")
-echo $DSYM
-ls
-sentry-cli --auth-token $SENTRY_TOKEN upload-dif --org henry-moule --project health-and-movement $DSYM
-# ios/Pods/FirebaseCrashlytics/upload-symbols -gsp ios/HealthAndMovement/GoogleService-Info.plist -p ios $DSYM
