c58e7064b1250a3feb4ff83cbaed90edbdbf7024
Henry Moule <henry.moule@zenobe.com>
2024-08-17 07:39:07 +0100

    fix: Added sentry to metro config

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,7 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+const {withSentryConfig} = require('@sentry/react-native/metro');
+
 const defaultSourceExts =
   require('metro-config/src/defaults/defaults').sourceExts;
 const defaultAssetExts =
@@ -11,18 +14,20 @@ const defaultAssetExts =
  * @type {import('metro-config').MetroConfig}
  */
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), {
-  transformer: {
-    babelTransformerPath: require.resolve('react-native-svg-transformer'),
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-  resolver: {
-    assetExts: defaultAssetExts.filter(ext => ext !== 'svg'),
-    sourceExts: [...defaultSourceExts, 'svg'],
-  },
-});
+module.exports = withSentryConfig(
+  mergeConfig(getDefaultConfig(__dirname), {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+    },
+    resolver: {
+      assetExts: defaultAssetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...defaultSourceExts, 'svg'],
+    },
+  }),
+);
