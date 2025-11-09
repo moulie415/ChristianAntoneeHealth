c7ecc0aae9dc5049580db5b4d7e205d49f9f09f4
Henry Moule <henry.moule@zenobe.com>
2025-11-09 09:01:35 +0000

    fix: workflow changes

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,15 @@ import 'dotenv/config';
 
 module.exports = ({ config }: ConfigContext) => ({
   ...config,
+  version: process.env.VERSION || '1.0.0',
+  ios: {
+    ...config.ios,
+    buildNumber: process.env.RUN_NUMBER || 1,
+  },
+  android: {
+    ...config.android,
+    versionCode: process.env.RUN_NUMBER || 1,
+  },
   plugins: [
     ...(config.plugins
       ? [
